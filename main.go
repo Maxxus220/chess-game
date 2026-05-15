8d1a59e7dafd060fca37c166f8c989acafd1052c
Michael Feist <maxxus220@gmail.com>
2026-05-14 22:38:17 -0500

    Swapped to logging and working on square selection

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/json"
 	"github.com/starfederation/datastar-go/datastar"
 	"html/template"
+	"log"
 	"net/http"
 )
+
+type DatastarSignals struct {
+	SelectedSquare string `json:"selectedSquare"`
+}
 
 const chessboardTmplString = `
 <div id="chess-board">
 	{{range $rank_index, $rank := .Ranks}}
 		{{range $file_index, $file := $.Files}}
-		<div class="chess-square" style="background-color: {{getSquareColor $rank_index $file_index}};"></div>
+		<div class="chess-square" data-on:click="$selectedSquare = {{$file}}{{$rank}}; @post('/api/select-square');"  style="background-color: {{getSquareColor $rank_index $file_index}};"></div>
 		{{end}}
 	{{end}}
 </div>
@@ -39,18 +44,31 @@ var chessboardTmpl = template.Must(template.New("board").Funcs(template.FuncMap{
 }).Parse(chessboardTmplString))
 
 func chessboardHandler(w http.ResponseWriter, r *http.Request) {
+	log.Default().Printf("Chessboard handler.\n")
 	data := struct {
 		Ranks []int
-		Files []int
+		Files []rune
 	}{
 		Ranks: []int{0, 1, 2, 3, 4, 5, 6, 7},
-		Files: []int{0, 1, 2, 3, 4, 5, 6, 7},
+		Files: []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
 	}
 	var templateResult bytes.Buffer
-	chessboardTmpl.Execute(&templateResult, data)
+	if err := chessboardTmpl.Execute(&templateResult, data); err != nil {
+		log.Default().Printf("Error executing template: %v\n", err)
+		return
+	}
 
 	sse := datastar.NewSSE(w, r)
 	sse.PatchElements(templateResult.String())
+}
+
+func selectSquareHandler(w http.ResponseWriter, r *http.Request) {
+	var signals DatastarSignals
+	if err := json.NewDecoder(r.Body).Decode(&signals); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	log.Default().Printf("Here with val %s\n", signals.SelectedSquare)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -68,8 +86,9 @@ func main() {
 
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/api/chess-board", chessboardHandler)
-	fmt.Println("Server starting on :8080")
+	http.HandleFunc("/api/select-square", selectSquareHandler)
+	log.Default().Println("Server starting on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Printf("Server failed: %s\n", err)
+		log.Default().Printf("Server failed: %s\n", err)
 	}
 }
